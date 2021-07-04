dfb9b674a74f0a72e56d2fb1065e764f8dec032f
Samuel Versiane Silva <samuellversiane@gmail.com>
2021-07-03 23:44:01 -0300

    finishBackend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,11 +2,12 @@ const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
-const { Mongoose } = require('mongoose')
 const server = express()
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extend: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`)
