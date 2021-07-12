a018ceba75dfa56c68914dac8ec69160331a1003
Samuel Versiane Silva <samuellversiane@gmail.com>
2021-07-12 20:41:40 -0300

    finishWithoutRedux

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -5,11 +5,11 @@ const express = require('express')
 const server = express()
 const allowCors = require('./cors')
 
-server.use(bodyParser.urlencoded({ extend: true }))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 
-server.listen(port, function () {
+server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
 })
 
